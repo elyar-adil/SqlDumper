b248d13a3e69f9628ef69b1682b41e68ffad48dd
Elyar Adil <e1y4r@outlook.com>
2022-01-20 16:50:10 +0800

    Update Main.java

--- src/main/java/me/elyar/Main.java
+++ src/main/java/me/elyar/Main.java
@@ -1,15 +1,11 @@
 package me.elyar;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import me.elyar.sqldumper.SqlDumper;
 import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila?user=root&password=password&serverTimezone=GMT%2B8");
-
         SqlDumper sqlDumper = new SqlDumper("jdbc:mysql://localhost:3306/sakila?user=root&password=password&serverTimezone=GMT%2B8");
         sqlDumper.dumpAllDatabase(System.out);
     }
